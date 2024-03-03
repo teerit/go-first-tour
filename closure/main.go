6cec94322edf4291d494f4a955e5604a47ca3b93
Teerit Ploensin <teerit.p@deverhood.com>
2024-03-04 00:35:47 +0700

    feat: add fibonacci function as a closure

--- closure/main.go
+++ closure/main.go
@@ -11,6 +11,14 @@ func createCounter() func() int {
 	return increment
 }
 
+func fibonacci() func() int {
+	x, y := 0, 1
+	return func() int {
+		x, y = y, x+y
+		return x
+	}
+}
+
 func main() {
 	counter1 := createCounter()
 	counter2 := createCounter()
@@ -23,4 +31,8 @@ func main() {
 	fmt.Println(counter2())
 	fmt.Println(counter2())
 
+	f := fibonacci()
+	for i := f(); i < 3; i = f() {
+		fmt.Println(i)
+	}
 }
